5ff0c1b0eb38419df67df9fc5fcd174caf44a7e6
Brayan Garcia <brayan.garcia@puntopay.co>
2021-11-10 19:58:17 -0500

    new views

--- soporte/views.py
+++ soporte/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import PersonaSoporteSerializer, PQRSerializer
+from .models import PersonaSoporte, PQR
 
 # Create your views here.
+
+class PersonaSoporteListCreate(generics.ListCreateAPIView):
+    queryset = PersonaSoporte.objects.all()
+    serializer_class = PersonaSoporteSerializer
+
+class PersonaSoporteUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = PersonaSoporte.objects.all()
+    serializer_class = PersonaSoporteSerializer
+
+class PQRListCreate(generics.ListCreateAPIView):
+    queryset = PQR.objects.all()
+    serializer_class = PQRSerializer
+
+class PQRUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = PQR.objects.all()
+    serializer_class = PQRSerializer
